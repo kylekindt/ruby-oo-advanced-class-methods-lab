6f6e6b7e7688af4382dceaa6adafceaa3792fee7
Kyle Kindt <k.kindt8@gmail.com>
2021-03-19 12:29:47 -0400

    commit tree 54fb2310fdb0b1d71bd724b1bfd1d60c535ddedd

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,57 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    self.all << song 
+    song 
+  end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(title)
+    song = self.create
+    song.name = title
+    song
+  end
+
+  def self.find_by_name(title)
+    self.all.detect{|s| s.name == title}
+  end
+
+  def self.find_or_create_by_name(title)
+    self.find_by_name(title) || self.create_by_name(title)
+  end
+
+  def self.alphabetical
+    self.all.sort_by{|s| s.name}
+  end
+
+  def self.new_from_filename(f_name)
+    f_array = f_name.split(" - ")
+    artist_name = f_array[0]
+    s_name = f_array[1].gsub(".mp3", "")
+    song = self.create
+    song.name = s_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(f_name)
+    f_array = f_name.split(" - ")
+    artist_name = f_array[0]
+    s_name = f_array[1].gsub(".mp3", "")
+    song = self.create
+    song.name = s_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
